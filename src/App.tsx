de0c79f2dd37ed7ed9e99ee213d2c5579d96258f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-22 22:28:35 +0900

    develop first sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import circle from './sketch/circle'
+import Canvas from './Canvas'
 
 const App = () => {
-  return <p>Hello World!</p>
+  return (
+    <>
+      <p>Hello World!</p>
+      <Canvas sketch={circle} />
+    </>
+  )
 }
 export default App
---
+++ src/sketch/circle.ts
@@ -0,0 +1,9 @@
+import p5 from 'p5'
+
+const circle = (p: p5) => {
+  p.setup = () => {}
+  p.draw = () => {
+    p.ellipse(50, 50, 80, 80)
+  }
+}
+export default circle
