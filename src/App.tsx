f86674e0eba35f5b0312bca47f9ea8d5a6389c0f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-23 09:18:50 +0900

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import * as sketch from './sketch'
 import Canvas from './components/Canvas'
-import Header from './components/Header'
 
 const App = () => {
   return (
     <>
-      <Header />
       <Canvas sketch={sketch.chapter1} />
     </>
   )
