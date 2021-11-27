ad6836fa8332e2be9e63496919a19492326e254d
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-11-27 15:49:37 +0900

    Fix code

--- src/pages/chapter3/Home.tsx
+++ src/pages/chapter3/Home.tsx
@@ -1,45 +1,29 @@
 const Chapter3 = () => {
+  const links = [
+    ['viewCircle', '画面中央に円を描画する'],
+    ['fillAndStroke', '塗りとストローク'],
+    ['fillAndStroke2', '塗りとストローク その2'],
+    ['mousePointer', 'マウス座標を利用する'],
+    ['mousePointerClear', '画面のクリア'],
+    ['mousePointer2', 'マウス座標を利用する その2'],
+    ['mousePointer3', 'マウス座標を利用する その3'],
+    ['random', 'ランダムを利用する'],
+    ['random2', 'ランダムを利用する その2'],
+    ['eclipse', '楕円の描画'],
+    ['eclipseExample', '楕円を利用した作例'],
+    ['eclipseExample2', '画面中央に円を描画する その2'],
+  ]
+
+  const list = links.map((link) => (
+    <li>
+      <a href={`/chapter3/${link[0]}`}>{link[1]}</a>
+    </li>
+  ))
+
   return (
     <main>
       <h1>chapter3:円の描画</h1>
-      <ul>
-        <li>
-          <a href='/chapter3/viewCircle'>画面中央に円を描画する</a>
-        </li>
-        <li>
-          <a href='/chapter3/fillAndStroke'>塗りとストローク</a>
-        </li>
-        <li>
-          <a href='/chapter3/fillAndStroke2'>塗りとストローク その2</a>
-        </li>
-        <li>
-          <a href='/chapter3/mousePointer'>マウス座標を利用する</a>
-        </li>
-        <li>
-          <a href='/chapter3/mousePointerClear'>画面のクリア</a>
-        </li>
-        <li>
-          <a href='/chapter3/mousePointer2'>マウス座標を利用する その2</a>
-        </li>
-        <li>
-          <a href='/chapter3/mousePointer3'>マウス座標を利用する その3</a>
-        </li>
-        <li>
-          <a href='/chapter3/random'>ランダムを利用する</a>
-        </li>
-        <li>
-          <a href='/chapter3/random2'>ランダムを利用する その2</a>
-        </li>
-        <li>
-          <a href='/chapter3/eclipse'>楕円の描画</a>
-        </li>
-        <li>
-          <a href='/chapter3/eclipseExample'>楕円を利用した作例</a>
-        </li>
-        <li>
-          <a href='/chapter3/eclipseExample2'>楕円を利用した作例 その2</a>
-        </li>
-      </ul>
+      <ul>{list}</ul>
     </main>
   )
 }
